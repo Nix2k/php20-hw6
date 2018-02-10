f9c0245c1504a9a66555561dca19e636a5ad223a
Nix2k <mihail.bokov@gmail.com>
2018-02-10 05:32:06 +0300

    v0.1

---
+++ admin.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Домашнее задание к лекции 2.2 «Обработка форм»</title>
+</head>
+<body>
+</body>
+</html>
